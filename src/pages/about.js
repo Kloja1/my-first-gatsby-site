de45ffe735ee3d3fb277c05d117de0ae3d01bb8c
Karl Lojander <Karl@MacBook-Pro.local>
2024-03-28 19:39:18 +0200

    Finished Gatsby Tutorial Part 4

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { graphql } from "gatsby"
 import Layout from '../components/layout'
+import Seo from '../components/seo'
 
 export const query = graphql`
   query {
@@ -22,6 +23,6 @@ const AboutPage = () => {
   )
 }
 
-export const Head = () => <title>About Me</title>
+export const Head = () => <Seo title="About Me" />
 
 export default AboutPage
